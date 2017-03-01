ee304872eb2095024d2f6d6573aa07e4474cba8c
sriwahyuni-aj <ajsriwahyuni@gmail.com>
2017-03-01 22:46:00 +0700

    Add files via upload

---
+++ pindahbaris.java
@@ -0,0 +1,7 @@
+class pindahbaris{
+	public static void main(String[] args) {
+		System.out.println("\t=================");
+		System.out.print("\tBelajar Java Yuks\n");
+		System.out.println("\t=================");
+	}
+}
